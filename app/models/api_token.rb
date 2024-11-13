6d9cc94c2a5c474e8c0fe8c829a255b59788351a
Júnior <b.sep@live.com>
2024-11-13 16:51:19 -0300

    active scope on api token model

--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class ApiToken < ApplicationRecord
-  validates :token, presence: true
+  before_validation :generate_token, on: :create
+
+  # validates :token, presence: true, uniqueness: { scope: :user_email }
   validates :user_email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
-  enum :active, { active: 0, inactive: 1 }
+  scope :active, -> { where(active: true) }
+
+  private
+
+  def generate_token
+    self.token = Digest::MD5.hexdigest(SecureRandom.hex)
+  end
 end
--- test/models/api_token_test.rb
+++ test/models/api_token_test.rb
@@ -3,18 +3,16 @@
 require 'test_helper'
 
 class ApiTokenTest < ActiveSupport::TestCase
-  context 'validations' do
-    should validate_presence_of(:token)
-  end
-
-  context 'define enum' do
-    should define_enum_for(:active)
-  end
-
   test 'validates email' do
     token = ApiToken.new(user_email: 'invalid', token: '1')
 
     assert_predicate(token, :invalid?)
     assert_equal(['User email is invalid'], token.errors.full_messages_for(:user_email))
+  end
+
+  test 'active scope' do
+    active_tokens = ApiToken.active
+
+    assert_not_includes(active_tokens, api_tokens(:two))
   end
 end
